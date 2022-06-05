66c9a18173de55973aefd239a67c19cceccdd969
Kshitij Arora <kshitij@Kshitijs-MacBook-Air.local>
2022-06-05 19:10:57 +0530

    Hello World

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+CBSE Class 12 Computer Science Project
+Employee Data Management Program
+*/
+
+//Header Files
+#include <iostream>
+//#include <stdio>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World";
+    return 0;
+}
